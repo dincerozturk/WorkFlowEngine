9857bedd0f36fcfe33df4118359e40b9c4dc87bd
dincerozturk <dincer.ozturk@hotmail.com>
2022-12-11 23:20:31 +0300

    IBaseTable

--- WorkflowManager.Common.EF6/Tables/BaseTable.cs
+++ WorkflowManager.Common.EF6/Tables/BaseTable.cs
@@ -1,14 +1,27 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 
 namespace WorkFlowManager.Common.Tables
 {
-    public abstract class BaseTable
+    public interface IBaseTable
+    {
+        int Id { get; set; }
+        DateTime CreatedTime { get; set; }
+        DateTime? UpdatedTime { get; set; }
+        DateTime? LastlyModifiedTime { get; }
+
+        ICollection<Document> Documents { get; set; }
+        ICollection<WorkFlowTrace> WorkFlowTraces { get; set; }
+    }
+
+
+    public abstract class BaseTable : IBaseTable
     {
         public int Id { get; set; }
         public DateTime CreatedTime { get; set; }
         public DateTime? UpdatedTime { get; set; }
-        public DateTime? LastlyModifiedTime => UpdatedTime ?? CreatedTime;
+        public DateTime? LastlyModifiedTime { get { return UpdatedTime ?? CreatedTime; } }
 
         public virtual ICollection<Document> Documents { get; set; }
         public virtual ICollection<WorkFlowTrace> WorkFlowTraces { get; set; }
