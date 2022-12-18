35ee4287093e5a23f6c025188e67dccaad950af3
dincerozturk <dincer.ozturk@hotmail.com>
2022-12-18 21:07:23 +0300

    klasörleme işlemi

---
+++ NET7/WFE.Core.DAO/Tables/BusinessProcess.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkFlowManager.Common.Tables
+{
+    public class BusinessProcess : BaseTable
+    {
+        public int? OwnerId { get; set; }
+        public BusinessProcess Owner { get; set; }
+        public string Name { get; set; }
+
+        public int? OwnerSubProcessTraceId { get; set; }
+        public WorkFlowTrace OwnerSubProcessTrace { get; set; }
+
+        public int? RelatedTaskId {  get; set; }
+        public Task RelatedTask { get; set; }
+    }
+}
---
+++ NetMvc/WorkFlowManager.Common/EF6/Tables/FormView.cs
@@ -0,0 +1,21 @@
+﻿using WorkFlowManager.Common.Enums;
+
+namespace WorkFlowManager.Common.Tables
+{
+    public class FormView : BaseTable
+    {
+        public int TaskId { get; set; }
+        public virtual Task Task { get; set; }
+
+        public string FormName { get; set; } //Harcama Talimatı Formu
+        public string FormDescription { get; set; } //Harcama Talimatı Formu
+
+        public string ViewName { get; set; } //HarcamaTalimatiFormu
+
+        public virtual FormComplexity FormComplexity { get; set; }
+
+        public int NumberOfPage { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
